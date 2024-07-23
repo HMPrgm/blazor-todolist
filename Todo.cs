2fcfff84f10b0769dd5c21f336b8374d5974d6d0
MSI Laptop <henrymunprof@gmail.com>
2024-07-23 13:38:07 -0400

    Add project files.

---
+++ Todo.cs
@@ -0,0 +1,20 @@
+﻿namespace ToDoList
+{
+    public class Todo
+    {
+        public string Name { get; init; }
+        public bool completed { get; set; }
+
+        public Todo(string name)
+        {
+            this.Name = name;
+            completed = false;
+        }
+
+        public bool ToggleCompleted()
+        {
+            completed = !completed;
+            return completed;
+        }
+    }
+}
